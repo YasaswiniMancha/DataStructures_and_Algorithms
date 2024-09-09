14ce40d4c9e2d23f03aaa737bb38c9edeaa96a81
YasaswiniMancha <yasaswinigayathri@gmail.com>
2024-09-09 14:59:57 +0530

    added new

--- DSA_java/src/stack/Stack.java
+++ DSA_java/src/stack/Stack.java
@@ -2,4 +2,67 @@ package stack;
 
 public class Stack {
 
+	int height;
+	Node top;
+	
+	class Node{
+		int value;
+		Node next;
+		
+		Node(int value){
+			this.value=value;
+		}
+		
+	}
+	
+	public Stack(int value) {
+		Node newNode =new Node(value);
+		top=newNode;
+		height=1;
+	}
+
+	public void getHeight() {
+		System.out.println("Height: "+height);
+	}
+
+
+	public void getTop() {
+		System.out.println("Top: "+top.value);
+	}
+
+	
+	public void printStack() {
+		System.out.println("printing stack:");
+		Node temp=top;
+		while(temp!=null) {
+			System.out.println(temp.value);
+			temp=temp.next;
+		}
+	}
+	
+	
+	public void push(int value) {
+		Node temp= top;
+		Node newNode=new Node(value);
+		if(height==0) {
+			top=newNode;
+		} else {
+		   newNode.next=top;
+		   top=newNode;
+		}
+	}
+	
+	public Node pop() {
+		Node temp=top;
+        if(height==0) {
+        	return null;
+        } else {
+        top=top.next;
+        temp.next=null;
+        }
+        height--;
+        return temp;
+	}
+	
+	
 }
--- DSA_java/src/stack/StackMain.java
+++ DSA_java/src/stack/StackMain.java
@@ -1,5 +1,20 @@
 package stack;
 
 public class StackMain {
-
+    public static void main(String args[]) {
+    	Stack st= new Stack(1);
+    	st.printStack();
+    	st.getHeight();
+    	st.getTop();
+    	
+    	st.push(2);
+    	st.push(3);
+    	st.push(4);
+    	System.out.println("After pushing elements: ");
+    	st.printStack();
+    	
+    	st.pop();
+    	System.out.println("After poping element: ");
+    	st.printStack();
+    }
 }
